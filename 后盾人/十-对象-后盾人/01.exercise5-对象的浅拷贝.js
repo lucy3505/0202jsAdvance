0c0cffec973599b2b55eded69cca8e86545cd32b
xuyanyu <13022112163@163.com>
2020-02-20 20:18:08 +0800

    chage

--- 后盾人/十-对象-后盾人/01.exercise5-对象的浅拷贝.js
+++ 后盾人/十-对象-后盾人/01.exercise5-对象的浅拷贝.js
@@ -6,4 +6,12 @@ cms.name = '1111'
 //要不被改变怎么处理?
 
 
-
+//``方法一
+let cm1 = Object.assign({}, hd)
+//``方法二：如果属性多不灵活
+let cm1 = { name: hd.name }
+//``方法二加强版：
+let obj = {}
+for (const key in hd) {
+  obj[key] = hd[key]
+}
